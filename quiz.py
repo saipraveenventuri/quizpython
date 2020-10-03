66c76d8d9b8fa4d57fe32c770d86f2dac16579dc
unknown <saipraveen14jan1996@gmail.com>
2020-10-03 15:24:59 +0530

    Added a function to create quiz object

--- quiz.py
+++ quiz.py
@@ -1,8 +1,19 @@
-import xml.etree.ElementTree as ET
 import os
 from datetime import datetime
 import pickle
 
+#provides log functionality for debugging
+#2 Modes have been provided log and err.
+def log(logtext,mode="log"):
+    if not os.path.exists("logs/"):
+        os.mkdir("logs")
+    if mode == "err":
+        file = open("logs/error.log","a")
+    else:
+        file = open("logs/app.log","a")
+    file.write(str(datetime.now())+"------------"+logtext+"\n")
+    file.close()
+    
 #quiz-class
 class quiz:
     def __init__(self,qname="",questions=[],options=[]):
@@ -11,18 +22,25 @@ class quiz:
         self.options = options
     def __str__(self):
         return 'quiz(name='+self.qname+', numberof questions='+str(len(self.questions))+ ')'
-    def validate(self):
+    def is_valid(self):
         if self.qname != "" and len(self.questions) != 0 and len(self.questions) == len(self.options):
-            return 1
-        return 0        
+            return True
+        return False
+    # returns -1 if file already exists,-2 for unable to create and 1 to success
     def store(self):
-        if self.validate():
+        if self.is_valid():
             if not os.path.exists("obj/"):
                 os.mkdir("obj")
-            with open("obj/"+self.qname+'.pkl', 'wb') as output:
+            if os.path.exists("obj/"+self.qname+".pkl"):
+                log("Quiz with same file exists","err")
+                return -1
+            with open("obj/"+self.qname+".pkl", 'wb') as output:
                 pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
+                return 1
         else:
-            print("invalid/null object cannot store")
+            log("invalid/null object cannot store","err")
+            return -2
+
 #fetches quiz object
 def fetch(qname):
     if os.path.exists("obj/"+qname+".pkl"):
@@ -30,23 +48,6 @@ def fetch(qname):
             quiz = pickle.load(input)
             return quiz
     return None
-
-
-#provides log functionality for debugging
-def logger(logtext):
-    if not os.path.exists("logs/"):
-        os.mkdir("logs")
-    log = open("logs/app.log","a")
-    log.write(str(datetime.now())+"------------"+logtext+"\n")
-    log.close()
-
-#provides error logs
-def error(errortext):
-    if not os.path.exists("logs/"):
-        os.mkdir("logs")
-    log = open("logs/error.log","a")
-    log.write(str(datetime.now())+"---"+errortext+"\n")
-    log.close()
     
 #creates necessary directories
 def initialize():
@@ -55,54 +56,38 @@ def initialize():
         os.mkdir("qxml")
         logger("qxml directory created")        
 
-#creates xml file with the parameters quizname,numberofquestions,listofquestions,options and answers
-def createquizxml(qname,qno,question,options,answers):
-    if qno>0 and qno == len(question) and qno == len(options) and qno == len(answers):
-        ##Making the quiz name as the base tag for the xml and the name of the xml file.
-        root = ET.Element(qname)
-        for i in range(1,qno+1):
-            questions = ET.SubElement(root, 'questions')
-            questions.set('id',str(i))
-            q = ET.SubElement(questions,'q')
-            q.text = question[i-1]
-            for j in range(1,len(options[i-1])+1):
-                opt=ET.SubElement(questions,'opt')
-                opt.set('id',str(j))
-                if j == answers[i-1]:
-                    opt.set('correct',str(1))
-                else:
-                    opt.set('correct',str(0))
-                opt.text=options[i-1][j-1]
-        mydata = ET.tostring(root).decode("utf-8")
-        mydata = mydata.replace(">",">\n")
-        mydata = mydata.replace("<","\n<")
-        myfile = open("qxml/"+qname+".xml", "w+")
-        myfile.write("<?xml version=\"1.0\"?>\n"+mydata)
-        logger("<admin> created quiz named "+qname+" with "+str(qno)+" questions")
-        myfile.close()
-        return 1
-    return 0
+#Command line function to create a quiz object
+def createQuiz():
+    try:
+        quizName = input("Enter the quiz name:")
+        numberOfQuestions = input("Enter number of questions:")
+        questions_list = []
+        options_list = []
 
-#This function validates the useroptions with correct answers
-def validate(useranswers,qname):
-    correct=[]
-    score = -1 
-    if os.path.exists("qxml/"+qname+".xml"):
-        tree = ET.parse("qxml/"+qname+".xml")
-        root = tree.getroot()
-        for child in root:
-            for subchild in child:
-                if subchild.tag == "opt" and subchild.attrib['correct'] == str(1):
-                    correct.append(int(subchild.attrib['id']))
-        if len(correct) == len(useranswers):
-            score = 0
-            for i in range(len(correct)):
-                if correct[i] == useranswers[i]:
-                    score = score +1
-        else:
-            error("number of answers expected:"+str(len(correct))+" Number of answers given:"+str(len(useranswers)))
-    else:
-        error("Quiz does not Exist")
-    return score
+        for i in range(0,int(numberOfQuestions)):
+            question = input("Enter " + str((i+1)) + " question:")
+            questions_list.append(question)
+            option1= input("Enter 1st option")
+            option2= input("Enter 2nd option")
+            option3= input("Enter 3rd option")
+            option4= input("Enter 4th option")
+            options = [option1,option2,option3,option4]
+            options_list.append(options)
 
-              
+        q = quiz(quizName, questions_list, options_list)
+
+        if q.is_valid()and q.store() == 1 :
+            return q
+        else :
+            return None
+    except:
+        log("Some problem in creating the Quiz",mode="err")
+        return None
+
+
+if __name__ == "__main__":
+    q = createQuiz()
+    if q is not None:
+        print(q);
+        
+    
